b94fd32642ae2dc6b02803a134d0712484cc7cd0
KasunShakthi <2001shakthikasun@gmail.com>
2025-02-25 11:47:27 +0530

    Initial commit

---
+++ src/main/java/com/example/virtual_city/VirtualCityApplication.java
@@ -0,0 +1,13 @@
+package com.example.virtual_city;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualCityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/virtual_city/VirtualCityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.virtual_city;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
